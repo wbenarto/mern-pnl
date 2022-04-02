194b414eb5fc524574571676b91769f05e24178f
w.benarto <w.benarto@gmail.com>
2022-04-01 17:11:14 -0700

    user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,27 @@
+// @desc Register new user
+// @route POST /api/user
+// @access Public
 const registerUser = (req, res) => {
     res.json({message: 'New User Registered'})
-
 }
 
+// @desc Authenticate user
+// @route POST /api/login
+// @access Public
+const loginUser = (req, res) => {
+    res.json({message: 'Login user'})
+}
+
+// @desc Get user data
+// @route GET /api/user/me
+// @access Public
+const getMe = (req, res) => {
+    res.json({message: 'User Data'})
+}
+
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser,
+    getMe,
 }
