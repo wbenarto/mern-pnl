9c4fbdbf04185fb997d496567f1cef5b25e7476c
w.benarto <w.benarto@gmail.com>
2022-04-01 17:19:52 -0700

    async handler user, jwt, bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,28 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require("bcryptjs")
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
 // @desc Register new user
 // @route POST /api/user
 // @access Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     res.json({message: 'New User Registered'})
-}
+})
 
 // @desc Authenticate user
 // @route POST /api/login
 // @access Public
-const loginUser = (req, res) => {
+const loginUser =asyncHandler(async (req, res) => {
     res.json({message: 'Login user'})
-}
+})
 
 // @desc Get user data
 // @route GET /api/user/me
 // @access Public
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.json({message: 'User Data'})
-}
+})
 
 
 module.exports = {
