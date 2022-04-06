0fe71ecb016ee55365ad84e37d31119f103db860
w.benarto <w.benarto@gmail.com>
2022-04-05 23:02:53 -0700

    header component

---
+++ frontend/src/component/Header.jsx
@@ -0,0 +1,12 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+function Header() {
+    return (
+        <div>
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+export default Header
