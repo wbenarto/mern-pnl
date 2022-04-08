d858c31cc233ccb951d2f25442ad14721f3d29b2
w.benarto <w.benarto@gmail.com>
2022-04-07 17:08:05 -0700

    header section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Header from './component/Header'
 
 function App() {
   return (
     <>
     <Router>
       <div classname='container'>
+        <Header />
         <Routes>
           <Route path='/' element={<Dashboard />} />
           <Route path='/login' element={<Login />} />
--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -3,9 +3,24 @@ import { Link } from 'react-router-dom'
 
 function Header() {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header className='header'>
+            <div className='logo'>
+                <Link to='/'>Goal Setter</Link>
+            </div>
+            <ul>
+                <li>
+                    <Link to='/login'>
+                        <FaSignInAlt /> Login
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/register'>
+                        <FaUser /> Register
+                    </Link>
+                </li>
+            </ul>
+
+        </header>
     )
 }
 
